ac21368b25723f3b13b760e0f872dd02abc8f929
jackson <s97403002a@gmail.com>
2023-12-20 23:48:45 +0800

    Create Author and Book table

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -7,3 +7,13 @@ using (var context = new PubContext())
 {
     context.Database.EnsureCreated();
 }
+
+
+using (var context = new PubContext())
+{
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}

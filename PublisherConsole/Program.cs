bf000e44e4e6fe86652405f28277bd519c9f0600
jackson <s97403002a@gmail.com>
2023-12-23 14:38:41 +0800

    Add QueryFilters and SkipAndTakeAuthors

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,21 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
 using Microsoft.EntityFrameworkCore;
 using PublisherData;
 using PublisherDomain;
 
 Console.WriteLine("Hello, World!");
+var pubContext1 = new PubContext();
 
 // using (var context = new PubContext())
 // {
 //     context.Database.EnsureCreated();
 // }
 
-QueryAuthors();
+// QueryAuthors();
 // AddAuthor();
 // QueryAuthors();
 
 // AddAuthorWithBook();
 // GetAuthorsWithBooks();
+
+// QueryFilters();
+// AddSomeMoreAuthors();
+SkipAndTakeAuthors();
+
+
+
+
+void SkipAndTakeAuthors()
+{
+    var pageSize = 2;
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Page " + i);
+        pubContext1.Authors.Skip(i*pageSize).Take(pageSize).ToList().ForEach(a =>
+        {
+            
+            Console.WriteLine(a.FirstName + " " + a.LastName);
+        });
+    }
+}
+
+void AddSomeMoreAuthors()
+{
+    pubContext1.Authors.Add(new Author(){FirstName = "Rowan", LastName = "Lerman"});
+    pubContext1.Authors.Add(new Author(){FirstName = "Don", LastName = "Jones"});
+    pubContext1.Authors.Add(new Author(){FirstName = "Jim", LastName = "Christopher"});
+    pubContext1.Authors.Add(new Author(){FirstName = "Stepthen", LastName = "Haunts"});
+    pubContext1.SaveChanges();
+}
+
+void QueryFilters()
+{
+    pubContext1.Authors.Where(r => r.FirstName == "Julie").ToList();
+
+    var julie = "Julie";
+    pubContext1.Authors.Where(r => r.FirstName == julie).ToList();
+}
 
 void GetAuthorsWithBooks()
 {
